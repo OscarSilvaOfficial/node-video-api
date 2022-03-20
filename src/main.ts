54224d33d40459bed629efb66c641098b3b32bd2
OscarSilvaOfficial <OscarSilvaOfficial>
2022-03-20 00:10:38 -0300

    Adição de cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { VideoController } from './adapters/controllers/video.controller';
 
 const app = express();
 const videoController = new VideoController();
+
+app.use(cors());
 
 app.get('/video', videoController.getVideo.bind(videoController));
 app.listen(5000, () => console.log('Example app listening on port 5000!'));
